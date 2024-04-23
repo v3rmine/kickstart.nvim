75deed64b722c67acf1ee375bcf3131bf532ded3
Johan Planchon <dev@johan.moe>
2024-04-23 15:19:54 +0200

    feat: saved persistant cursor

--- init.lua
+++ init.lua
@@ -176,6 +176,18 @@ vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_netrwSettings = 1
 vim.g.loaded_netrwFileHandlers = 1
 
+-- [[ Persistent Cursor ]]
+-- SOURCE: https://github.com/LunarVim/LunarVim/issues/4071
+vim.api.nvim_create_autocmd('BufReadPost', {
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
