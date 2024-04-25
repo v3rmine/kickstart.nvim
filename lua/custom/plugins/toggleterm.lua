de1dc6f46127979e898d7bbc8938152ed9755a52
Johan Planchon <dev@johan.moe>
2024-04-25 20:22:24 +0200

    feat: added terminal in buffer command

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -1,3 +1,12 @@
+vim.keymap.set('n', '<leader>ttb', function()
+  local original_win = vim.api.nvim_get_current_win()
+  vim.cmd 'split | term'
+  local term_buf = vim.api.nvim_get_current_buf()
+  vim.cmd 'setlocal nonumber norelativenumber'
+  vim.api.nvim_buf_set_keymap(term_buf, 'n', '<Esc>', '<cmd>close<CR>', { noremap = true, silent = true })
+  vim.api.nvim_win_close(original_win, false)
+end, { desc = '[t]oggle [t]erminal [b]uffer' })
+
 return {
   'akinsho/toggleterm.nvim',
   cmd = { 'ToggleTerm', 'ToggleTermToggleAll' },
@@ -5,12 +14,42 @@ return {
     -- SOURCE: https://github.com/akinsho/toggleterm.nvim
   },
   init = function()
+    local Terminal = require('toggleterm.terminal').Terminal
+
     -- Create a floating terminal
-    vim.keymap.set('n', '<leader>tt', ':ToggleTerm direction=float name=speedterm<CR>', { desc = '[t]oggle floating [t]erminal' })
+    local quickTerm = Terminal:new {
+      display_name = 'QuickTerm',
+      direction = 'float',
+      hidden = true,
+      on_open = function(term)
+        vim.cmd 'startinsert!'
+        vim.api.nvim_buf_set_keymap(term.bufnr, 'n', 'q', '<cmd>close<CR>', { noremap = true, silent = true })
+      end,
+    }
+    vim.keymap.set('n', '<leader>ttf', function()
+      quickTerm:toggle()
+    end, { desc = '[T]oggle [T]erminal [F]loating' })
 
-    vim.keymap.set('t', '<Esc>', '<C-\\><C-n>:q<CR>', { desc = 'Close floating terminal' })
+    local function close_on_esc(term)
+      vim.cmd 'startinsert!'
+      vim.api.nvim_buf_set_keymap(term.bufnr, 't', '<Esc>', '<cmd>close<CR>', { noremap = true, silent = true })
+    end
+    local hterm = Terminal:new {
+      direction = 'horizontal',
+      hidden = true,
+      on_open = close_on_esc,
+    }
+    vim.keymap.set('n', '<leader>tth', function()
+      hterm:toggle()
+    end, { desc = '[T]oggle [T]erminal [H]orizontal' })
 
-    vim.keymap.set('n', '<leader>tht', ':ToggleTerm direction=horizontal name=horizterm<CR>', { desc = '[t]oggle [h]orizontal [t]erminal' })
-    vim.keymap.set('n', '<leader>tvt', ':ToggleTerm direction=vertical name=vertterm<CR>', { desc = '[t]oggle [v]ertical [t]erminal' })
+    local vterm = Terminal:new {
+      direction = 'vertical',
+      hidden = true,
+      on_open = close_on_esc,
+    }
+    vim.keymap.set('n', '<leader>ttv', function()
+      vterm:toggle()
+    end, { desc = '[T]oggle [T]erminal [V]ertical' })
   end,
 }
