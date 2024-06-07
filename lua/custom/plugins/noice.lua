fb1efb72d52aa3d09f30dbff77c1dc0207332efa
Johan Planchon <johan.planchon@simplx.fr>
2024-06-07 15:12:58 +0200

    feat: updated noice config

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -2,7 +2,22 @@ return {
   'folke/noice.nvim',
   event = 'VeryLazy',
   opts = {
-    -- add any options here
+    lsp = {
+      -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+      override = {
+        ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+        ['vim.lsp.util.stylize_markdown'] = true,
+        ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+      },
+    },
+    -- you can enable a preset for easier configuration
+    presets = {
+      bottom_search = true, -- use a classic bottom cmdline for search
+      command_palette = true, -- position the cmdline and popupmenu together
+      long_message_to_split = true, -- long messages will be sent to a split
+      inc_rename = false, -- enables an input dialog for inc-rename.nvim
+      lsp_doc_border = false, -- add a border to hover docs and signature help
+    },
   },
   dependencies = {
     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
@@ -12,4 +27,10 @@ return {
     --   If not available, we use `mini` as the fallback
     'rcarriga/nvim-notify',
   },
+  init = function()
+    require('notify').setup {
+      timeout = 2000,
+      stages = 'static',
+    }
+  end,
 }
