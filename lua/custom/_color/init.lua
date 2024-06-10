77d47c01e15bf7a52496ef47f1439867ac36227e
Johan Planchon <johan.planchon@simplx.fr>
2024-06-10 11:06:32 +0200

    feat: persistent colorscheme nvim

---
+++ lua/custom/_color/init.lua
@@ -0,0 +1,25 @@
+local M = {}
+
+local colorscheme_file = '~/.config/nvim/lua/custom/_color/colorscheme.lua'
+
+M.setup = function()
+  vim.cmd [[colorscheme default]]
+
+  -- try load custom._color.colorscheme and if not found create the file with current colorscheme
+  local ok, set_colourscheme = pcall(require, 'custom._color.colorscheme')
+  if ok then
+    set_colourscheme()
+  else
+    vim.fn.jobstart('echo "return function()\n  vim.cmd [[colorscheme ' .. vim.g.colors_name .. ']]\nend" > ' .. colorscheme_file)
+  end
+
+  vim.api.nvim_create_autocmd('ColorScheme', {
+    callback = function(args)
+      vim.notify('Saved colorscheme: ' .. args.match, vim.log.levels.INFO, { title = 'ColorScheme' })
+      vim.fn.jobstart("sed -i '' -e 's/\\[\\[colorscheme .*\\]\\]/[[colorscheme " .. args.match .. "]]/' " .. colorscheme_file)
+      -- vim.fn.jobstart("sed -i '' -e 's/include colorscheme_.*/include colorscheme_" .. args.match .. "/' ~/.config/kitty/kitty.conf")
+    end,
+  })
+end
+
+return M
