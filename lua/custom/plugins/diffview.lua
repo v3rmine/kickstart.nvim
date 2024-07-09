ba3f27c3db8c4f1adccbeec48cc50a1cb29bebce
Johan Planchon <johan.planchon@simplx.fr>
2024-07-09 16:12:41 +0200

    feat: added neogit and removed diffview default keymaps

--- lua/custom/plugins/diffview.lua
+++ lua/custom/plugins/diffview.lua
@@ -1,4 +1,14 @@
 return {
   'sindrets/diffview.nvim',
   cmd = { 'DiffviewOpen' },
+  opts = {
+    view = {
+      merge_tool = {
+        layout = 'diff2_horizontal',
+      },
+    },
+    keymaps = {
+      disable_defaults = true,
+    },
+  },
 }
